5d84c38ad6451252947af77e8c97c8e87bf37447
sagar2000000 <sagar171joshi@gmail.com>
2025-03-26 13:49:26 +0545

    first commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,14 @@
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncuserDeletion, syncUserUpdation } from "@/config/inngest";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncuserDeletion
+
+   
+  ],
+});
--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { productsDummyData, userDummyData } from "@/assets/assets";
+import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
 
@@ -13,6 +14,8 @@ export const AppContextProvider = (props) => {
 
     const currency = process.env.NEXT_PUBLIC_CURRENCY
     const router = useRouter()
+
+    const {user} =useUser()
 
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
@@ -88,7 +91,8 @@ export const AppContextProvider = (props) => {
         products, fetchProductData,
         cartItems, setCartItems,
         addToCart, updateCartQuantity,
-        getCartCount, getCartAmount
+        getCartCount, getCartAmount,
+        user
     }
 
     return (
